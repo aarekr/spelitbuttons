a850832eec4f38f584cad1c08857b49fd32ac54c
aarekr <aare212@hotmail.com>
2026-07-12 16:03:09 +0300

    One circle appears randomly on the screen

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flame/game.dart';
 import 'package:flame/components.dart';
 import 'package:flame/events.dart';
 import 'package:get/get.dart';
+import 'dart:math';
 
 void main() {
   runApp(GameApp());
@@ -35,7 +36,7 @@ class StartScreen extends StatelessWidget {
                 "Start",
                 style: TextStyle(fontSize: 24),
               ),
-              onPressed: null,//() => Get.to(() => GameScreen()),
+              onPressed: () => Get.to(() => LevelScreen()),
             ),
           ],
         ),
@@ -43,3 +44,83 @@ class StartScreen extends StatelessWidget {
     );
   }
 }
+
+class LevelScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: 
+      Center(child:
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text("Choose level", style: TextStyle(fontSize: 30)), // onPressed: () => Get.to(() => GameScreen()),
+            ElevatedButton(child: Text("Level 1"), onPressed: () => Get.to(() => GameScreen())),
+            ElevatedButton(child: Text("Level 2"), onPressed: null),
+            ElevatedButton(child: Text("Level 3"), onPressed: null),
+          ],
+        ),
+      )
+    );
+  }
+}
+
+class GameScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GameWidget(
+      game: EasyGame(),
+    );
+  }
+}
+
+class EasyGame extends FlameGame {
+  var score = 0;
+  EasyGame() {
+    add(TapCircle1());
+  }
+  incrementScore() {
+    score++;
+    /*if (score >= 10) {
+      Get.offAll(() => ResultScreen(score: score));
+    }*/
+  }
+}
+
+class TapCircle1 extends CircleComponent with HasGameReference<EasyGame>, TapCallbacks, HasVisibility {
+  final Random random = Random();
+  double timeUntilNextAppearance = 2.0;
+  double visibleTime = 1.0;
+  TapCircle1()
+    : super(
+      position: Vector2(200, 200),
+      radius: 70,
+    ) {
+      paint.color = Colors.blue;
+      isVisible = false;
+    }
+  @override
+  void update(double dt) {
+    super.update(dt);
+    if (!isVisible) {
+      timeUntilNextAppearance -= dt;
+      if (timeUntilNextAppearance <= 0) {
+        isVisible = true;
+        visibleTime = 1.0;
+      }
+    } else {
+      visibleTime -= dt;
+      if (visibleTime <= 0) {
+        isVisible = false;
+        timeUntilNextAppearance = 1 + random.nextDouble() * 3;
+      }
+    }
+  }
+  @override
+  void onTapDown(TapDownEvent event) {
+    game.incrementScore();
+    isVisible = false;
+    timeUntilNextAppearance = 1 + random.nextDouble() * 3;
+  }
+}
+
