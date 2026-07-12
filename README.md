9242382ff49c8e1b9332211cce730d3527d97e51
aarekr <aare212@hotmail.com>
2026-07-12 14:47:41 +0300

    Repo and app setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# spelitbuttons
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/spelitbuttons/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.spelitbuttons
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
